37b788f42ae2aaeac674dffca4c78257a6163dbe
Lara <lararabanoh@gmail.com>
2022-09-22 11:18:28 +0200

    Lista de listas

--- gestor.py
+++ gestor.py
@@ -1,4 +1,17 @@
-from tabulate import tabulate 
+"""Soy un profe, y me llega una lista con las notas de mis alumnos de este curso.
+
+Quiero imprimir una lista con el equivalente de cada nota, estas siendo:
+
+Menos de 4 (4 incluido), suspenso; 5 aprobado; 6 bien; 7 y 8 notable; 9 y 10 sobresaliente. (Asume que la lista siempre contiene números del 0 al 10).
+
+Ejemplo:
+
+Entrada: [8, 4, 2, 9, 6, 7, 5, 0]
+
+Salida: [notable, suspenso, suspenso, sobresaliente, bien, notable, aprobado, suspenso]"""
+
+
+from tabulate import tabulate
 
 
 curso = [id_alumno := [], alumno := [], nota := [], calificación := []]
@@ -8,31 +21,30 @@ otra = "Y"
 
 while otra == "Y":
 
-
     id_alumno.append(int(input("Introduce el identificador del alumno: ")))
 
     alumno.append(str(input("Introduce un alumno: ")))
 
     nota.append(int(input("Introduce una nota: ")))
-        
-    otra = input("¿Quieres introducir otra entrada en el registro? (Y/N): ").upper()
-    
+
+    otra = input(
+        "¿Quieres introducir otra entrada en el registro? (Y/N): ").upper()
+
     while otra != "N" and otra != "Y":
         otra = input("Respuesta incorrecta. Introduce Y o N: ").upper()
-    
 
 
 for i in nota:
 
     if i <= 4:
         calificación.append("Suspenso")
-    
+
     elif i == 5:
         calificación.append("Aprobado")
 
     elif i == 6:
         calificación.append("Bien")
-    
+
     elif i == 7 or nota == 8:
         calificación.append("Notable")
 
@@ -40,8 +52,8 @@ for i in nota:
         calificación.append("Sobresaliente")
 
 
-print(tabulate(curso, headers=["Identificador", "Nombre", "Nota", "Calificación"]))
-
+print(tabulate(curso, headers=["Identificador",
+      "Nombre", "Nota", "Calificación"]))
 
 
 pregunta = input("¿Quieres modificar algún elemento de la tabla?(Y/N ")
@@ -51,9 +63,4 @@ while pregunta == "Y":
     """Planteamiento para buscar los datos que queremos modificar"""
 
     while pregunta != "N" and pregunta != "Y":
-            pregunta = input("Respuesta incorrecta. Introduce Y o N: ").upper()
-
-
-    
-
-    
+        pregunta = input("Respuesta incorrecta. Introduce Y o N: ").upper()

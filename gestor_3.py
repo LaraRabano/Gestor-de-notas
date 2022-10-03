049513be9d8e11f7afad46f510effa0c3c7857ed
Lara <lararabanoh@gmail.com>
2022-10-03 08:59:00 +0200

    Funciones

--- gestor_3.py
+++ gestor_3.py
@@ -1,57 +1,72 @@
+
 curso = []
-contador = 1
 list_notas = []
-alumn_nombre = ""
-alumn_nota = ""
+otra = "Y"
+contador = 0
+
+def nuevo_alumno(): #Cambiar formato nombre.
+    global contador
+    alumn_nombre = IntroducirNombre()
+    alumn_nota = IntroducirNota()
+    alumn_calificacion = ConversionNotas()
+    
+    alumno = {
+        "ID": contador,
+        "Nombre": alumn_nombre,
+        "Nota": alumn_nota,
+        "Calificacion": alumn_calificacion
+    }
+
+    contador = contador + 1
+    return alumno
 
 
-def NuevoAlumno():
+def IntroducirNombre():
     alumn_nombre = input("Introduce un alumno: ")
 
     while not alumn_nombre.isalpha():
         alumn_nombre = input("El nombre introducido no es correcto: ")
 
-        while True:
-            try:
-                alumn_nota = float(input("Introduce una nota: "))
-            except:
-                print("La nota introducida no es correcta.")
-                continue
-            if alumn_nota < 0 or alumn_nota > 10:
-                print("La nota introducida no es correcta.")
-                continue
-            break
-
-    alumno = {
-        "ID": contador,
-        "Nombre": alumn_nombre,
-        "Nota": alumn_nota,
-        "Calificacion": ConversionNotas()
-    }
-
-    ConversionNotas()
-    contador = contador + 1
-
-    # Los datos recogidos se añaden como diccionario a la lista "curso"
-    curso.append(alumno)
+    return alumn_nombre
 
 
-def ConversionNotas(alumn_nota):
+def IntroducirNota():
+    while True:
+        try:
+            alumn_nota = float(input("Introduce una nota: "))
+        except:
+            print("La nota introducida no es correcta.")
+            continue
+        if alumn_nota < 0 or alumn_nota > 10:
+            print("La nota introducida no es correcta.")
+            continue
+        break
 
-    if alumn_nota <= 4:
-        alumno["Calificacion"] = "Suspenso"
+    return alumn_nota
 
-    elif alumn_nota == 5:
-        alumno["Calificacion"] = "Aprobado"
 
-    elif alumn_nota == 6:
-        alumno["Calificacion"] = "Bien"
+def ConversionNotas():
 
-    elif alumn_nota == 7 or alumn_nota == 8:
-        alumno["Calificacion"] = "Notable"
+    alumn_calificacion = ""
 
-    elif alumn_nota == 9 or alumn_nota == 10:
-        alumno["Calificacion"] = "Sobresaliente"
+    nota = IntroducirNota()
+
+    if nota <= 4:
+        alumn_calificacion = "Suspenso"
+
+    elif nota == 5:
+        alumn_calificacion = "Aprobado"
+
+    elif nota == 6:
+        alumn_calificacion = "Bien"
+
+    elif nota == 7 or nota == 8:
+        alumn_calificacion = "Notable"
+
+    elif nota == 9 or nota == 10:
+        alumn_calificacion = "Sobresaliente"
+
+    return alumn_calificacion
 
 
 print(curso)
@@ -59,9 +74,9 @@ print(curso)
 option = """Elige una opción:
 1. Introducir un nuevo alumno.
 2. Modificar un elemento de la tabla.
-3. Eliminar un alumno"""
+3. Eliminar un alumno. :"""
 
-pregunta = input("¿Quieres hacer alguna modificación? (Y/N): ").upper
+pregunta = input("¿Quieres hacer alguna modificación? (Y/N): ").upper()
 
 while pregunta == "Y":
     respuesta = int(input(option))
@@ -69,14 +84,17 @@ while pregunta == "Y":
     # Añadir nuevo alumno.
     if respuesta == 1:
         while otra == "Y":
-            NuevoAlumno()
+            alumno = NuevoAlumno()
 
-        # Bucle para introducir más alumnos en la lista "curso"
-        otra = input(
-            "¿Quieres introducir otra entrada en el registro? (Y/N): ").upper()
+            # Los datos recogidos se añaden como diccionario a la lista "curso"
+            curso.append(alumno)
 
-        while otra != "N" and otra != "Y":
-            otra = input("Respuesta incorrecta. Introduce Y o N: ").upper()
+            # Bucle para introducir más alumnos en la lista "curso"
+            otra = input(
+                "¿Quieres introducir otra entrada en el registro? (Y/N): ").upper()
+
+            while otra != "N" and otra != "Y":
+                otra = input("Respuesta incorrecta. Introduce Y o N: ").upper()
 
     if respuesta == 2:
         while pregunta == "Y":
--- puebas y cosas/prueba2.py
+++ puebas y cosas/prueba2.py
@@ -1,4 +1,3 @@
-pregunta = input("¿Quieres hacer alguna modificación? (Y/N): ").upper
 
-while pregunta == "Y":
-    respuesta = int(input("option"))
+
+alumn_calificacion = ""
